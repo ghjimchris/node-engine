8eb6d8406f2630733ed72bdc9a05f1a0a4bae281
ghjimchris <jimrajchris@gmail.com>
2023-04-16 15:15:37 +0000

    Dummy Middleware

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
-const https = require('https');
+// const https = require('https');
 
 
-const server = https.createServer((req, res) => {
+// const server = https.createServer((req, res) => {
     
-});
+// });
 
-server.listen(3000)
+// server.listen(3000)
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+    console.log('middleware');
+})
+
+app.use((req, res, next) => {
+    console.log('another midware');
+})
+
+app.listen(3000)
